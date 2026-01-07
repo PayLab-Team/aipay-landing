20c1b2e5c8c3d534670446b174f2d6824644d842
Aidyn Torgayev <aidyn.torgayev@gmail.com>
2026-01-07 18:18:08 +0500

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin', 'cyrillic'] });
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="ru" suppressHydrationWarning>
       <body className={`${inter.className} min-h-screen flex flex-col`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
